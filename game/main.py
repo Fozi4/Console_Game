529a0c8398ce033742016c5ab9719cac1d4bce08
Fozi4 <bajmaran@gmail.com>
2025-06-16 14:53:54 +0200

    Abilities created, and shown

--- game/main.py
+++ game/main.py
@@ -1,22 +1,32 @@
 class Player:
-    def __init__(self, name,hp,attack,mana):
+    def __init__(self, name,hp,attack,mana,ability):
         self.name = name
         self.hp = hp
         self.attack = attack
         self.mana = mana
+        self.ability = ability
+    def stats(self):
+        return f"{self.name} stats: \nhp: {self.hp} \ndmg:{self.attack} \nmana:{self.mana} \nability: {self.ability}"
     
 class Warrior(Player):
-    def __init__(self,name, hp, attack,mana):
-        super().__init__(name,hp,attack,mana)
-        block = block
-
+    def __init__(self,name, hp, attack,mana,block):
+        super().__init__(name,hp,attack,mana,block)
+    
 class Archer(Player):
-    def __init__(self,name,hp,attack,mana):
-        super().__init__(name,hp,attack,mana)
-        double_attack = double_attack
+    def __init__(self,name,hp,attack,mana,double_attack):
+        super().__init__(name,hp,attack,mana,double_attack)
 
 class Mage(Player):
-    def __init__(self,name,hp,attack,mana):
-        super().__init__(name,hp,attack,mana)
-        mana_regen = mana_regen
-        fireball = fireball
+    def __init__(self,name,hp,attack,mana,fireball):
+        super().__init__(name,hp,attack,mana,fireball)
+
+warrior = Warrior("Warrior",100,10,50,"Block")
+
+archer = Archer("Archer", 90,20,70,"Double Attack")
+
+mage = Mage("Mage", 60,40,100,"Fireball")
+
+stats = [warrior.stats(),archer.stats(),mage.stats()]
+for i in stats:
+    print(f"{i} \n")
+
