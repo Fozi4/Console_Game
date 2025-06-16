e1f1b75b2343011df0fc295c95ebb0638473114c
HavermAn2 <olejnikandrej04@gmail.com>
2025-06-16 16:01:00 +0200

    Enemy+character choose

--- game/main.py
+++ game/main.py
@@ -7,10 +7,20 @@ class Player:
         self.ability = ability
     def stats(self):
         return f"{self.name} stats: \nhp: {self.hp} \ndmg:{self.attack} \nmana:{self.mana} \nability: {self.ability}"
+    def take_damage(self,dmg):
+        if self.hp - dmg <=0:
+            return "You died!!!"
+        
+        self.hp -=dmg
+        return f"Remaining health {self.hp}"
+    
+
+    
     
 class Warrior(Player):
     def __init__(self,name, hp, attack,mana,block):
         super().__init__(name,hp,attack,mana,block)
+    
     
 class Archer(Player):
     def __init__(self,name,hp,attack,mana,double_attack):
@@ -19,14 +29,24 @@ class Archer(Player):
 class Mage(Player):
     def __init__(self,name,hp,attack,mana,fireball):
         super().__init__(name,hp,attack,mana,fireball)
+    
 
-warrior = Warrior("Warrior",100,10,50,"Block")
 
-archer = Archer("Archer", 90,20,70,"Double Attack")
 
-mage = Mage("Mage", 60,40,100,"Fireball")
+warrior = Warrior("0-Warrior",100,10,50,"Block")
+
+archer = Archer("1-Archer", 90,20,70,"Double Attack")
+
+mage = Mage("2-Mage", 60,40,100,"Fireball")
 
 stats = [warrior.stats(),archer.stats(),mage.stats()]
 for i in stats:
     print(f"{i} \n")
+
+choose = input("Choose your character from 0 to 2:")
+characters = [warrior, archer, mage]
+character=characters[choose]
+print(character)
+
+
 
