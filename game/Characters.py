ad0706917834b4912b68d00bd667188026257f2b
HavermAn2 <olejnikandrej04@gmail.com>
2025-06-16 21:52:51 +0200

    combat

--- game/Characters.py
+++ game/Characters.py
@@ -1,4 +1,4 @@
-from Combat import combat
+
 class Player:
     def __init__(self, name,hp,attack,mana,ability):
         self.name = name
@@ -8,14 +8,20 @@ class Player:
         self.ability = ability
     def stats(self):
         return f"{self.name} stats: \nhp: {self.hp} \ndmg:{self.attack} \nmana:{self.mana} \nability: {self.ability}"
-    def take_damage(self,dmg):
-        if self.hp - dmg <=0:
-            combat = False
-            return "You died!!!"
-
-        self.hp -=dmg
-        return f"Remaining health {self.hp}"
+    def is_alive(self):
+        return self.hp > 0
     
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        
+    
+    def normal_attack(self, target):
+        
+        target.take_damage(self.attack)
+        return str(f"{self.name} attacks {target.name} for {self.attack} damage!")
+    
+    def use_ability(self, target):
+        pass  
     def __str__(self):
         return f"You've chosen {self.name}"
     
@@ -23,15 +29,32 @@ class Player:
 class Warrior(Player):
     def __init__(self,name, hp, attack,mana,block):
         super().__init__(name,hp,attack,mana,block)
+
     
     
 class Archer(Player):
     def __init__(self,name,hp,attack,mana,double_attack):
         super().__init__(name,hp,attack,mana,double_attack)
+    def use_ability(self, target):
+        if self.mana >= 10:
+            dmg = self.attack * 2
+            print(f"{self.name} uses Double Attack for {dmg} damage!")
+            target.take_damage(dmg)
+            self.mana -= 10
+        else:
+            print(f"{self.name} has not enough mana!")
 
 class Mage(Player):
     def __init__(self,name,hp,attack,mana,fireball):
         super().__init__(name,hp,attack,mana,fireball)
+    def use_ability(self, target):
+        if self.mana >= 20:
+            dmg = self.attack + 20
+            print(f"{self.name} uses Fireball for {dmg} damage!")
+            target.take_damage(dmg)
+            self.mana -= 20
+        else:
+            print(f"{self.name} has not enough mana!")
     
 
 
@@ -46,8 +69,8 @@ stats = [warrior.stats(),archer.stats(),mage.stats()]
 for i in stats:
     print(f"{i} \n")
 
-choose = int(input("Choose your character from 0 to 2:"))
-characters = [warrior, archer, mage]
-character=characters[choose]
-print(character)
+# choose = int(input("Choose your character from 0 to 2:"))
+# characters = [warrior, archer, mage]
+# character=characters[choose]
+# print(character)
 
--- game/Combat.py
+++ game/Combat.py
@@ -1,20 +1,78 @@
 from random import randint
-from Enemies import *
-from Characters import * 
-attacks = ["attack", Player.ability]
-combat = True
-turn = randint(1,2)
-guess = input("Guess a number to decide who goes first(1 or 2): ")
-if turn == guess:
-    print("You go first")
-    your_turn = True
-else:
-    print("You go second")
-    your_turn = False
-while combat:
-    if your_turn == True:
-        for i in attacks:
-            print(i,end=" ")
-        type_of_attack = input("Choose type of attack")
-        combat = False
+from Characters import Player,Warrior,Archer,Mage
+from Enemies import Enemy
+import os
+
+#--------------------Characters-------------------------------#
+warrior = Warrior("Warrior",100,10,50,"Block")
+archer = Archer("Archer",80,15,10,"Double Attack")
+mage = Mage("Mage",60,20,70,"Fireball")
+#-------------------Enemies--------------------------------#
+enemy = Enemy("Ork",50,5,10,"bite")
+#-------------------------------------------------------------#
+_choose = True
+messages = []
+messages.append("-----ACTIONS------")
+# _characters=[]
+# _characters.append(warrior,archer,mage)
+
+def show_messeges(messeges):
+        for i in messages:
+            print(" "*50+i)
+
+
+
+
+
+
+
+def combat(player, enemy):
+    turn = 1
+    while player.is_alive() and enemy.is_alive():
+        print(f"\n------------ Turn {turn} ---------") 
+        print(show_messeges(messages))
+
+        print(f"{player.name} HP: {player.hp}, Mana: {player.mana}"+"\n")
+        print("\n" + " "*10+"VS"+"\n")
+        print(f"{enemy.name} HP: {enemy.hp}")
         
+
+        action = input("Choose action: (1) Attack (2) Ability: ")
+        if action == "1":
+            
+            messages.append(player.normal_attack(enemy))
+        elif action == "2":
+            player.use_ability(enemy)
+        else:
+            print("Invalid choise< ti dolboeb")
+
+        if not enemy.is_alive():
+            print(f"{enemy.name} is defeated!")
+            break
+        
+        
+        enemy.normal_attack(player)
+        if not player.is_alive():
+            print(f"{player.name} is defeated!")
+            break
+        
+        turn += 1
+        os.system("cls" if os.name == "nt" else "clear")
+
+
+while _choose:
+    INPUT = int(input("CHOOSE YOUR CHAMPION:\n1 - Warrior\n2 - Archer\n3 - Mage\n"))
+    if INPUT == 1:
+        os.system("cls" if os.name == "nt" else "clear")
+        combat(warrior, enemy)
+        _choose = False
+    if INPUT == 2:
+        os.system("cls" if os.name == "nt" else "clear")
+        combat(archer, enemy)
+        _choose = False
+    if INPUT == 3:
+        os.system("cls" if os.name == "nt" else "clear")
+        combat(mage, enemy)
+        _choose = False
+    else:
+        print("Enter a correct number")
