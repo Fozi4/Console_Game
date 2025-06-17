63d9fb071a54be531146c54e773b6d9d370e3cde
Fozi4 <bajmaran@gmail.com>
2025-06-17 13:33:55 +0200

    target choose

--- game/Enemies.py
+++ game/Enemies.py
@@ -6,7 +6,7 @@ class Enemy(Player):
     def stats(self):
         return f"{self.name} stats: \nhp: {self.hp} \ndmg:{self.attack} \nmana:{self.mana} \nability:{self.ability}"
 enemy = Enemy("Ork",50,5,10,"bite")
-enemy1=Enemy("Ork",50,5,10,"bite")
+enemy1 = Enemy("Ork",50,5,10,"bite")
 stats = [enemy.stats()]
 for i in stats:
     print(f"{i} \n")
